c9b59c5ab44c860f180ad4e1fd60503c9d6b4a6f
paiabhishekpai <abhishekupai1124@gmail.com>
2024-10-07 20:45:20 +0530

    initialising

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <div className="container bg-red-800">
+      I am red and i am div
+     </div>
+    </>
+  )
+}
+
+export default App

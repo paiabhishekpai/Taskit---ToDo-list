0c1bac3217eaaee12d2b7ace829a89a7c488981f
paiabhishekpai <abhishekupai1124@gmail.com>
2024-10-07 21:37:46 +0530

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
+import { useState } from "react";
+import Navbar from "./components/navbar";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-     <div className="container bg-red-800">
-      I am red and i am div
-     </div>
+    <Navbar/>
+    <div className="container mx-auto my-5 rounded-xl p-5 bg-blue-100">
+    <h1 className="text-xl font-bold">Your ToDos</h1>
+          <div className="todos">
+           <div className="todo">
+
+           </div>
+          </div>
+        </div>
     </>
   )
 }
 
-export default App
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="flex justify-between py-2 bg-blue-600 text-white">
+      <div className="logo p-3">
+        <span className="font-bold text-xl">TaskIt</span>
+      </div>
+      <ul className='flex gap-5 p-3'>
+        <li className='cursor-pointer hover:font-bold transition-all'>Home</li>
+        <li className='cursor-pointer hover:font-bold transition-all'>Your tasks</li>
+      </ul>
+    </nav>
+    </div>
+  )
+}
+
+export default Navbar
